9f31807332a506dfca28e507cdf93da19d268149
Rodrigo Oliveira <rodrigo.dealer@gmail.com>
2020-05-13 17:33:41 +0200

    Reading config

--- config_test.go
+++ config_test.go
@@ -15,7 +15,9 @@ environment:
 - ONE_ENV=hello_set
 - TWO_ENV=hello_set`
 	var c = Config{}
+	
 	c.Load([]byte(data))
+
 	assert.Equal(t, "vault", c.Provider.Name, "they should be equal")
 	assert.Equal(t, 2, len(c.Environment), "they should be equal")
 }
