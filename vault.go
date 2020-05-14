55de45b9552904527228bc3e4c73fb7d420e3e1f
Rodrigo Oliveira <rodrigo.oliveira@secretescapes.com>
2020-05-14 18:31:55 +0200

    Vault initial setup

---
+++ vault.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"strings"
+)
+
+type EnvironmentVariable struct {
+	Name  string
+	Value string
+}
+
+func GetEnvs(envs []string) []EnvironmentVariable {
+	m := []EnvironmentVariable{}
+	for _, element := range envs {
+		m = append(m, GetEnvParts(element))
+	}
+	return m
+}
+
+func GetEnvParts(env string) EnvironmentVariable {
+	parts := strings.Split(env, "=")
+	return EnvironmentVariable{Name: parts[0], Value: parts[1]}
+}
---
+++ vault_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetEnvs(t *testing.T) {
+	var theArray []string = []string{"VAR_A=bla", "VAR_B=bla"}
+
+	var values = GetEnvs(theArray)
+
+	assert.Equal(t, 2, len(values), "they should be equal")
+	assert.Equal(t, "bla", values[0].Value, "they should be equal")
+}
+
+func TestGetEnvParts(t *testing.T) {
+	var theString = "VAR_A=bla"
+
+	var env = GetEnvParts(theString)
+
+	assert.Equal(t, "bla", env.Value, "they should be equal")
+}
