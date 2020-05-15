62d72545c56895f174ae9bef227458b3ae8617f7
Rodrigo Oliveira <rodrigo.dealer@gmail.com>
2020-05-15 11:07:08 +0200

    Stuff

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.14
 
 require (
 	github.com/aws/aws-sdk-go v1.30.25 // indirect
-	github.com/hashicorp/vault/api v1.0.4 // indirect
+	github.com/hashicorp/vault/api v1.0.4
 	github.com/kpango/glg v1.5.1
 	github.com/stretchr/testify v1.5.1
 	gopkg.in/yaml.v2 v2.2.8
--- vault.go
+++ vault.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"github.com/hashicorp/vault/api"
+	"github.com/kpango/glg"
 	"strings"
 )
 
@@ -20,4 +22,37 @@ func GetEnvs(envs []string) []EnvironmentVariable {
 func GetEnvParts(env string) EnvironmentVariable {
 	parts := strings.Split(env, "=")
 	return EnvironmentVariable{Name: parts[0], Value: parts[1]}
+}
+
+func getOnVault() {
+
+}
+
+type Vault struct {
+	Client *api.Logical
+}
+
+func (m *Vault) New(addr string) *api.Client {
+	config := &api.Config{
+		Address: addr,
+	}
+	client, err := api.NewClient(config)
+	if err != nil {
+		glg.Errorf("Error connecting to Vault: %s", err.Error())
+	}
+	return client
+}
+
+func (m *Vault) Connect(token string, addr string) {
+	client := m.New(addr)
+	client.SetToken(token)
+	m.Client = client.Logical()
+}
+
+func (m *Vault) GetSecret(secretName string) string {
+	secret, err := m.Client.Read(secretName)
+	if err != nil {
+		glg.Errorf("Error getting secret in Vault: %s", err.Error())
+	}
+	return secret.Data["hello"].(string)
 }
